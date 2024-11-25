2307cb22a917a6d052f7036b17798e4f77079850
CONNORPC\k1lle <connorcomeau98@gmail.com>
2024-11-25 14:28:21 -0800

    Removed Upsetti page, added minecraft tutorial

--- js/main.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-    $.stellar({
-        verticalScrolling: true,
-        parallaxBackgrounds: true,
-        responsive: true,
-        scrollProperty: 'scroll',
-        positionProperty: 'position'
-    }).stellar('refresh');
-});
